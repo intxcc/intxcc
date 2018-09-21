c719209281a990aa69315ab20ca0d15c01ddf18a
Marvin Alexander Rüll <dev@intx.cc>
2018-09-21 21:35:06 +0200

    Adjust logo colors.

--- src/logo/LogoPath.js
+++ src/logo/LogoPath.js
@@ -3,43 +3,73 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const LogoPath = (props) => (
-  <svg className={props.className} width="250.65" height="217.06" version="1.1" viewBox="0 0 66.318 57.429" xmlns="http://www.w3.org/2000/svg">
-    <defs>
-      <filter id="filter4840" colorInterpolationFilters="sRGB">
-        <feColorMatrix result="color2" values="-1 0 0 0 1 0 0 -1 0 1 0 -1 0 0 1 -0.21 -0.72 -0.07 2 0 ">
-        </feColorMatrix></filter>
-    </defs>
-    <g transform="matrix(2.1708 0 0 2.1708 -194.46 -182.16)" filter={props.invert ? 'url(#filter4840)' : ''}>
-      <g transform="matrix(.83491 .0027989 -.0027989 .83491 16.445 13.609)">
-        <g transform="matrix(1.197 -.0040136 .0040136 1.197 70.129 -52.192)">
-          <g transform="translate(0,1.0583)" fill="#fff">
-            <path d="m14.446 112.69 15.284 26.472 15.284-26.472z" />
-            <path d="m16.393 113.76 13.337 23.273 13.437-23.273z" stroke="#242124" strokeWidth=".3" />
-          </g>
-          <g transform="matrix(.37943 0 0 .37943 26.892 74.962)">
-            <g transform="matrix(.85121 0 0 .85121 -36.697 21.734)" fill="none" stroke="#242124" strokeMiterlimit="3.8" strokeWidth=".3098">
-              <path d="m34.91 105.52v14.143h5.8778z" />
-              <path d="m68.982 119.67 0.10498-10.127h-4.7877z" />
-              <path d="m71.623 105.52v2.9938h-7.6412z" />
-              <path d="m56.476 107.73h-5.1555l5.1555 8.9274z" />
-              <path d="m49.555 107.73v8.9274h5.1703z" />
-            </g>
-            <g transform="matrix(2.5339,5.9913e-4,5.9913e-4,-2.5339,402.71,-32.573)" fill="#242124" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} aria-label="i n t x · c c">
-              <path d="m-160.13-63.052h-0.18293v-1.2041h0.18293z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
-              <path d="m-158.51-63.052h-0.1819l-0.49299-0.90537v0.90537h-0.1819v-1.2041h0.1819l0.49299 0.9064v-0.9064h0.1819z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
-              <path d="m-157.83-64.098 0.33693 1.59e-4 -4.9e-4 1.0459 0.18294 8.7e-5 4.9e-4 -1.0459 0.33486 1.58e-4 8e-5 -0.15813-0.85473-4.04e-4z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
-              <path d="m-155.93-63.806 0.23047-0.44958h0.21601l-0.33693 0.59738 0.34933 0.60668h-0.21704l-0.23978-0.45785-0.23771 0.45785h-0.216l0.34519-0.60668-0.33899-0.59738h0.21497z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
-              <path d="m-154.8-63.658q0-0.04961 0.0269-0.08268 0.0279-0.03307 0.0827-0.03307t0.0827 0.03307 0.0279 0.08268q0 0.04858-0.0279 0.08165-0.0279 0.03204-0.0827 0.03204t-0.0827-0.03204q-0.0269-0.03307-0.0269-0.08165z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal', paintOrder: 'stroke fill markers'}} />
-              <path d="m-152.98-63.441q-0.0186 0.1974-0.1292 0.30179-0.11058 0.10439-0.29352 0.10439-0.19844 0-0.31832-0.15296-0.11886-0.154-0.11886-0.41754v-0.09922q0-0.25735 0.12092-0.41238 0.12093-0.15503 0.3266-0.15503 0.1788 0 0.28628 0.10749 0.10853 0.10645 0.1261 0.30386h-0.18294q-0.0145-0.13436-0.0693-0.1943t-0.16019-0.05994q-0.13023 0-0.19741 0.10645-0.0672 0.10645-0.0672 0.30489v0.10335q0 0.19844 0.0641 0.30386t0.19017 0.10542q0.11059 0 0.1664-0.05478 0.0568-0.05581 0.0734-0.19534z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
-              <path d="m-151.46-63.441q-0.0186 0.1974-0.12919 0.30179t-0.29352 0.10439q-0.19844 0-0.31833-0.15296-0.11886-0.154-0.11886-0.41754v-0.09922q0-0.25735 0.12093-0.41238 0.12092-0.15503 0.32659-0.15503 0.1788 0 0.28629 0.10749 0.10852 0.10645 0.12609 0.30386h-0.18294q-0.0145-0.13436-0.0692-0.1943-0.0548-0.05994-0.1602-0.05994-0.13022 0-0.1974 0.10645-0.0672 0.10645-0.0672 0.30489v0.10335q0 0.19844 0.0641 0.30386t0.19017 0.10542q0.11058 0 0.16639-0.05478 0.0568-0.05581 0.0734-0.19534z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
-            </g>
-          </g>
-          <path d="m29.73 131.5 3.4159-5.9166h-6.8318z" fill="#242124" />
-          <path d="m29.73 130.71 2.7476-4.759h-5.4952z" fill="none" stroke="#fff" strokeWidth=".2" />
+const LogoNormal = () => (
+  <g transform="matrix(2.1708 0 0 2.1708 -194.46 -182.16)">
+    <g transform="matrix(.83491 .0027989 -.0027989 .83491 16.445 13.609)">
+      <g transform="matrix(1.197 -.0040136 .0040136 1.197 70.129 -52.192)">
+        <g transform="translate(0,1.0583)" fill="#fff">
+          <path d="m14.446 112.69 15.284 26.472 15.284-26.472z" />
+          <path d="m16.393 113.76 13.337 23.273 13.437-23.273z" stroke="#242124" strokeWidth=".3" />
         </g>
+        <g transform="matrix(.37943 0 0 .37943 26.892 74.962)">
+          <g transform="matrix(.85121 0 0 .85121 -36.697 21.734)" fill="none" stroke="#242124" strokeMiterlimit="3.8" strokeWidth=".3098">
+            <path d="m34.91 105.52v14.143h5.8778z" />
+            <path d="m68.982 119.67 0.10498-10.127h-4.7877z" />
+            <path d="m71.623 105.52v2.9938h-7.6412z" />
+            <path d="m56.476 107.73h-5.1555l5.1555 8.9274z" />
+            <path d="m49.555 107.73v8.9274h5.1703z" />
+          </g>
+          <g transform="matrix(2.5339,5.9913e-4,5.9913e-4,-2.5339,402.71,-32.573)" fill="#242124" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} aria-label="i n t x · c c">
+            <path d="m-160.13-63.052h-0.18293v-1.2041h0.18293z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+            <path d="m-158.51-63.052h-0.1819l-0.49299-0.90537v0.90537h-0.1819v-1.2041h0.1819l0.49299 0.9064v-0.9064h0.1819z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+            <path d="m-157.83-64.098 0.33693 1.59e-4 -4.9e-4 1.0459 0.18294 8.7e-5 4.9e-4 -1.0459 0.33486 1.58e-4 8e-5 -0.15813-0.85473-4.04e-4z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+            <path d="m-155.93-63.806 0.23047-0.44958h0.21601l-0.33693 0.59738 0.34933 0.60668h-0.21704l-0.23978-0.45785-0.23771 0.45785h-0.216l0.34519-0.60668-0.33899-0.59738h0.21497z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+            <path d="m-154.8-63.658q0-0.04961 0.0269-0.08268 0.0279-0.03307 0.0827-0.03307t0.0827 0.03307 0.0279 0.08268q0 0.04858-0.0279 0.08165-0.0279 0.03204-0.0827 0.03204t-0.0827-0.03204q-0.0269-0.03307-0.0269-0.08165z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal', paintOrder: 'stroke fill markers'}} />
+            <path d="m-152.98-63.441q-0.0186 0.1974-0.1292 0.30179-0.11058 0.10439-0.29352 0.10439-0.19844 0-0.31832-0.15296-0.11886-0.154-0.11886-0.41754v-0.09922q0-0.25735 0.12092-0.41238 0.12093-0.15503 0.3266-0.15503 0.1788 0 0.28628 0.10749 0.10853 0.10645 0.1261 0.30386h-0.18294q-0.0145-0.13436-0.0693-0.1943t-0.16019-0.05994q-0.13023 0-0.19741 0.10645-0.0672 0.10645-0.0672 0.30489v0.10335q0 0.19844 0.0641 0.30386t0.19017 0.10542q0.11059 0 0.1664-0.05478 0.0568-0.05581 0.0734-0.19534z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+            <path d="m-151.46-63.441q-0.0186 0.1974-0.12919 0.30179t-0.29352 0.10439q-0.19844 0-0.31833-0.15296-0.11886-0.154-0.11886-0.41754v-0.09922q0-0.25735 0.12093-0.41238 0.12092-0.15503 0.32659-0.15503 0.1788 0 0.28629 0.10749 0.10852 0.10645 0.12609 0.30386h-0.18294q-0.0145-0.13436-0.0692-0.1943-0.0548-0.05994-0.1602-0.05994-0.13022 0-0.1974 0.10645-0.0672 0.10645-0.0672 0.30489v0.10335q0 0.19844 0.0641 0.30386t0.19017 0.10542q0.11058 0 0.16639-0.05478 0.0568-0.05581 0.0734-0.19534z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+          </g>
+        </g>
+        <path d="m29.73 131.5 3.4159-5.9166h-6.8318z" fill="#242124" />
+        <path d="m29.73 130.71 2.7476-4.759h-5.4952z" fill="none" stroke="#fff" strokeWidth=".2" />
       </g>
     </g>
+  </g>
+)
+
+const LogoDark = () => (
+  <g transform="matrix(2.1708 0 0 2.1708 -194.46 -182.16)">
+    <g transform="matrix(.9994 -7.1148e-7 7.1148e-7 .9994 75.142 -29.77)">
+      <g transform="translate(0,1.0583)">
+        <path d="m14.446 112.69 15.284 26.472 15.284-26.472z" fill="#242124" />
+        <path d="m16.393 113.76 13.337 23.273 13.437-23.273z" fill="none" stroke="#fff" strokeWidth=".3" />
+      </g>
+      <g transform="matrix(.37943 0 0 .37943 26.892 74.962)">
+        <g transform="matrix(.85121 0 0 .85121 -36.697 21.734)" fill="none" stroke="#fff" strokeMiterlimit="3.8" strokeWidth=".3098">
+          <path d="m34.91 105.52v14.143h5.8778z" />
+          <path d="m68.982 119.67 0.10498-10.127h-4.7877z" />
+          <path d="m71.623 105.52v2.9938h-7.6412z" />
+          <path d="m56.476 107.73h-5.1555l5.1555 8.9274z" />
+          <path d="m49.555 107.73v8.9274h5.1703z" />
+        </g>
+        <g transform="matrix(2.5339,5.9913e-4,5.9913e-4,-2.5339,402.71,-32.573)" fill="#fff" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} aria-label="i n t x · c c">
+          <path d="m-160.13-63.052h-0.18293v-1.2041h0.18293z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+          <path d="m-158.51-63.052h-0.1819l-0.49299-0.90537v0.90537h-0.1819v-1.2041h0.1819l0.49299 0.9064v-0.9064h0.1819z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+          <path d="m-157.83-64.098 0.33693 1.59e-4 -4.9e-4 1.0459 0.18294 8.7e-5 4.9e-4 -1.0459 0.33486 1.58e-4 8e-5 -0.15813-0.85473-4.04e-4z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+          <path d="m-155.93-63.806 0.23047-0.44958h0.21601l-0.33693 0.59738 0.34933 0.60668h-0.21704l-0.23978-0.45785-0.23771 0.45785h-0.216l0.34519-0.60668-0.33899-0.59738h0.21497z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+          <path d="m-154.8-63.658q0-0.04961 0.0269-0.08268 0.0279-0.03307 0.0827-0.03307t0.0827 0.03307 0.0279 0.08268q0 0.04858-0.0279 0.08165-0.0279 0.03204-0.0827 0.03204t-0.0827-0.03204q-0.0269-0.03307-0.0269-0.08165z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal', paintOrder: 'stroke fill markers'}} />
+          <path d="m-152.98-63.441q-0.0186 0.1974-0.1292 0.30179-0.11058 0.10439-0.29352 0.10439-0.19844 0-0.31832-0.15296-0.11886-0.154-0.11886-0.41754v-0.09922q0-0.25735 0.12092-0.41238 0.12093-0.15503 0.3266-0.15503 0.1788 0 0.28628 0.10749 0.10853 0.10645 0.1261 0.30386h-0.18294q-0.0145-0.13436-0.0693-0.1943t-0.16019-0.05994q-0.13023 0-0.19741 0.10645-0.0672 0.10645-0.0672 0.30489v0.10335q0 0.19844 0.0641 0.30386t0.19017 0.10542q0.11059 0 0.1664-0.05478 0.0568-0.05581 0.0734-0.19534z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+          <path d="m-151.46-63.441q-0.0186 0.1974-0.12919 0.30179t-0.29352 0.10439q-0.19844 0-0.31833-0.15296-0.11886-0.154-0.11886-0.41754v-0.09922q0-0.25735 0.12093-0.41238 0.12092-0.15503 0.32659-0.15503 0.1788 0 0.28629 0.10749 0.10852 0.10645 0.12609 0.30386h-0.18294q-0.0145-0.13436-0.0692-0.1943-0.0548-0.05994-0.1602-0.05994-0.13022 0-0.1974 0.10645-0.0672 0.10645-0.0672 0.30489v0.10335q0 0.19844 0.0641 0.30386t0.19017 0.10542q0.11058 0 0.16639-0.05478 0.0568-0.05581 0.0734-0.19534z" style={{fontFeatureSettings: 'normal', fontVariantCaps: 'all-small-caps', fontVariantLigatures: 'normal', fontVariantNumeric: 'normal'}} />
+        </g>
+      </g>
+      <path d="m29.73 131.5 3.4159-5.9166h-6.8318z" fill="#fff" />
+      <path d="m29.73 130.71 2.7476-4.759h-5.4952z" fill="#fff" stroke="#242124" strokeWidth=".2" />
+    </g>
+  </g>
+)
+
+const LogoPath = (props) => (
+  <svg className={props.className} width="250.65" height="217.06" version="1.1" viewBox="0 0 66.318 57.429" xmlns="http://www.w3.org/2000/svg">
+    {props.invert ? <LogoDark /> : <LogoNormal />}
   </svg>
 )
 

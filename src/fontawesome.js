2f7ee0e1b4012d63b44543f1c52beef40ba89153
Marvin Alexander Rüll <dev@intx.cc>
2018-09-23 18:24:55 +0200

    Add move anchor for everybody not using the scroll wheel.

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -1,11 +1,13 @@
 'use strict'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelopeOpen, faArrowsAlt } from '@fortawesome/free-solid-svg-icons'
 // import { faTwitter } from '@fortawesome/free-regular-svg-icons'
 import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 library.add(faEnvelopeOpen)
+library.add(faArrowsAlt)
+
 library.add(faTwitter)
 library.add(faGithub)
 library.add(faLinkedin)
--- src/view/Skills/SkillsMap.js
+++ src/view/Skills/SkillsMap.js
@@ -3,6 +3,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import { observer } from 'mobx-react'
 
 import SkillsMapColumn from './SkillsMapColumn'
@@ -10,18 +12,28 @@ import SkillsMapCategory from './SkillsMapCategory'
 import SkillsMapItem from './SkillsMapItem'
 
 const SkillsMap = observer((props) => (
-  <div className='skills-map-wrapper'>
-    {props.columns.map(column => (
-      <SkillsMapColumn key={'skills-column-' + column.title} title={column.title}>
-        {column.categories.map(category => (
-          <SkillsMapCategory key={'skills-category-' + column.title + '-' + category.title} title={category.title}>
-            {category.skills.map(skill => (
-              <SkillsMapItem key={'skills-item-' + column.title + '-' + category.title + '-' + skill.title} title={skill.title} />
-            ))}
-          </SkillsMapCategory>
-        ))}
-      </SkillsMapColumn>
-    ))}
+  <div className='skills-map-outer-wrapper'>
+    <div className='skills-map-wrapper'>
+      {props.columns.map(column => (
+        <SkillsMapColumn key={'skills-column-' + column.title} title={column.title}>
+          {column.categories.map(category => (
+            <SkillsMapCategory key={'skills-category-' + column.title + '-' + category.title} title={category.title}>
+              {category.skills.map(skill => (
+                <SkillsMapItem key={'skills-item-' + column.title + '-' + category.title + '-' + skill.title} title={skill.title} />
+              ))}
+            </SkillsMapCategory>
+          ))}
+        </SkillsMapColumn>
+      ))}
+    </div>
+    <div className='skills-map-scroll-anchor'>
+      <div className='skills-map-scroll-anchor-symbol'>
+        <FontAwesomeIcon icon={'arrows-alt'} />
+      </div>
+      <div className='skills-map-scroll-anchor-caption'>
+        move
+      </div>
+    </div>
   </div>
 ))
 

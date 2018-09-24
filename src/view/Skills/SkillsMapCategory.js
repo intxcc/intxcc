4f64da1f1c9574efe9f9fb4ea5614eeda75fb6ba
Marvin Alexander Rüll <dev@intx.cc>
2018-09-24 13:28:51 +0200

    Select categories in a pretty way.

--- src/view/Skills/SkillsMapCategory.js
+++ src/view/Skills/SkillsMapCategory.js
@@ -9,17 +9,19 @@ import SkillsMapItem from './SkillsMapItem'
 
 const SkillsMapCategory = observer((props) => (
   <div className={'skills-map-category' + (props.selected ? ' category-selected' : '')}>
-    <h2>
-      {props.title}
-    </h2>
-    {props.skills.map(skill => (
-      <SkillsMapItem
-        key={'skills-' + skill.id}
-        selected={props.selected.skill && props.selected.skill.id === skill.id}
-        centerMapFunc={props.centerMapFunc}
-        title={skill.title}>
-      </SkillsMapItem>
-    ))}
+    <div className={'skills-map-category-inner'}>
+      <h2>
+        {props.title}
+      </h2>
+      {props.skills.map(skill => (
+        <SkillsMapItem
+          key={'skills-' + skill.id}
+          selected={props.selected.skill && props.selected.skill.id === skill.id}
+          centerMapFunc={props.centerMapFunc}
+          title={skill.title}>
+        </SkillsMapItem>
+      ))}
+    </div>
   </div>
 ))
 

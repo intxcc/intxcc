3e8ede5ed10bd81ccfa6bf65b40e1a674b0b34c3
Marvin Alexander Rüll <dev@intx.cc>
2018-10-10 14:25:01 +0200

    Removed pre-disable. And animating of clip-path while disabled for better UX.

--- src/mobx/StateData/StartpageData.js
+++ src/mobx/StateData/StartpageData.js
@@ -3,15 +3,14 @@
 const StartpageData = {
   basicInfo: {
     id: 'startpageBasicInfo',
-    disabled: true
-    // popups: [{
-    //   id: 'start-popup',
-    //   className: 'start-popup',
-    //   persistent: false,
-    //   title: 'This is intxcc.',
-    //   text: 'This is future: This is life: This is freedom: This is warm summer rain: This is design: This is for you.',
-    //   hint: 'This is development, design and art from Berlin.'
-    // }]
+    popups: [{
+      id: 'start-popup',
+      className: 'start-popup',
+      persistent: false,
+      title: 'This is intxcc.',
+      text: 'This is future: This is life: This is freedom: This is warm summer rain: This is design: This is for you.',
+      hint: 'This is development, design and art from Berlin.'
+    }]
   }
 }
 

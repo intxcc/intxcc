444411a3521503e94c6cb83aa9b3f932d071e45f
Marvin Alexander Rüll <dev@intx.cc>
2018-10-11 15:07:44 +0200

    No, I love edge. Why are you asking?

--- src/config/POPUP_SKILLS_EXPLANATION.js
+++ src/config/POPUP_SKILLS_EXPLANATION.js
@@ -5,7 +5,7 @@ const SKILLS_EXPLANATION = {
   className: 'skills-explanation',
   title: 'Explanation of skills',
   text: 'The skills are rated from 1 to 3, where 1 is basic, 2 is good and 3 is very good knowledge.',
-  hint: 'The rating is shown on the left side of each skill.'
+  hint: 'The rating is shown on the left side of each skill. To filter skills use the menu on the right side of the logo.'
 }
 
 export default SKILLS_EXPLANATION

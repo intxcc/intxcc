c0881c433cfbbcb2daf767b6add02bb050b7a51d
Marvin Alexander Rüll <dev@intx.cc>
2018-10-11 13:43:48 +0200

    I love edge so much that it even gets a polyfill from me <3

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ import store from './mobx/store'
 
 import App from './App'
 
+// ///////// //
+// POLYFILLS //
+
+import smoothscroll from 'smoothscroll-polyfill'
+smoothscroll.polyfill()
+
+// /////////// //
+// RENDER APP //
+
 ReactDOM.render(
   <App store={store} />,
   document.getElementById('app')

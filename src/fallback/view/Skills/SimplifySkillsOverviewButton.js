1bed587622acf453fd487ee8ab6f4b762bf0ab88
Marvin Alexander Rüll <dev@intx.cc>
2018-10-14 18:55:36 +0200

    Add even more responsivity.

--- src/fallback/view/Skills/SimplifySkillsOverviewButton.js
+++ src/fallback/view/Skills/SimplifySkillsOverviewButton.js
@@ -6,8 +6,10 @@ import PropTypes from 'prop-types'
 import { observer } from 'mobx-react'
 
 const SimplifySkillsOverviewButton = observer(props => (
-  <div className='fallback-skills-simplify-overview-button-wrapper' onClick={() => props.setFallbackUseSkillMapFunc(!props.fallbackUseSkillMap)}>
-    {props.fallbackUseSkillMap ? 'Click here to use normal presentation.' : 'Click here to use simplified presentation.'}
+  <div className='fallback-skills-simplify-overview-button-wrapper'>
+    <div className='fallback-skills-simplify-overview-button' onClick={() => props.setFallbackUseSkillMapFunc(!props.fallbackUseSkillMap)}>
+      {props.fallbackUseSkillMap ? 'Click here to use normal presentation.' : 'Click here to use simplified presentation.'}
+    </div>
   </div>
 ))
 

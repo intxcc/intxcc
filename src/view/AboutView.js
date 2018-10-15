fb34e44047b2a88d0e14a48db2890d010b3ecd9d
Marvin Alexander Rüll <dev@intx.cc>
2018-10-15 20:39:48 +0200

    Changed picture in about to fallback as well. It just has more style. TODO I might use it on the normal startpage as well.

--- src/view/AboutView.js
+++ src/view/AboutView.js
@@ -22,7 +22,7 @@ AboutView.propTypes = {
 
 const AboutOverlayView = observer((props) => (
   <div className='overlay-wrapper-inner'>
-    <img className='about-picture' alt='Picture of me with triangles.' src='/pic.png' />
+    <img className='about-picture' alt='Picture of me with triangles.' src='/fallback_pic.png' />
     <div className='about-page-name-display'>
       about me
       <div className='about-page-name-display-caption'>

cb4b99226254bc58619af341dae446ac465a7d67
Marvin Alexander Rüll <dev@intx.cc>
2018-10-16 18:57:38 +0200

    Give the fallback skill details close button style.

--- src/fallback/view/Skills/FallbackSkillDetails.js
+++ src/fallback/view/Skills/FallbackSkillDetails.js
@@ -3,12 +3,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import { observer } from 'mobx-react'
 
 const FallbackSkillDetails = observer(props => (
   <div className={'fallback-skill-details-wrapper' + (props.show ? ' show' : '')}>
     <div onClick={props.closeSkillDetailsFunc} className='fallback-skill-details-close-button'>
-      +
+      <FontAwesomeIcon icon={'times'} />
     </div>
   </div>
 ))

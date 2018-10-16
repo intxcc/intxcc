9fad7b24812f8b5a3870f4ff880941abc8a409bc
Marvin Alexander Rüll <dev@intx.cc>
2018-10-16 23:22:58 +0200

    Added missing details to fallback skill details.

--- src/fallback/view/Skills/FallbackSkillDetails.js
+++ src/fallback/view/Skills/FallbackSkillDetails.js
@@ -7,13 +7,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { observer } from 'mobx-react'
 
+import MARK_TOOLTIP from '../../../config/MarkTooltips'
+
 const FallbackSkillDetails = observer(props => (
   <div className={'fallback-skill-details-wrapper' + (props.show ? ' show' : '')}>
     <div onClick={props.closeSkillDetailsFunc} className='fallback-skill-details-close-button'>
       <FontAwesomeIcon icon={'times'} />
     </div>
     <div className={'fallback-skill-details-inner' + (props.showInner ? ' show' : '')}>
-      <h1>{props.indexOfSelectedSkillInSelectedSkills} of <strong>{props.selectedListLength - 1}</strong> | {props.skill.title}</h1>
+      <div className='fallback-skill-details-inner-info'>
+        <h2>Skill {props.indexOfSelectedSkillInSelectedSkills} of <strong>{props.selectedListLength - 1}</strong></h2>
+        <h3>{MARK_TOOLTIP[props.skill.mark].toUpperCase()}</h3>
+        <h1>{props.skill.title}</h1>
+      </div>
+      <p className='fallback-skill-details-desc'>
+        {props.skill.desc}
+      </p>
+      <p className='fallback-skill-details-trivia'>
+        {props.skill.trivia}
+      </p>
       <div onClick={() => props.scrollSkillFunc(-1)} className='fallback-skill-details-inner-bottom-button previous'>
         <FontAwesomeIcon icon={'arrow-circle-left'} />
       </div>

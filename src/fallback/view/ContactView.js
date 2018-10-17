a54d4f2e1a36109347591f22319d75e374eeaccc
Marvin Alexander Rüll <dev@intx.cc>
2018-10-17 18:17:14 +0200

    Add impressum and social media links to fallback about.

--- src/fallback/view/ContactView.js
+++ src/fallback/view/ContactView.js
@@ -12,8 +12,8 @@ import LicenseAndImpressumLink from './Components/LicenseAndImpressumLink'
 const ContactView = observer(props => (
   <div className='fallback-view-wrapper contact-wrapper'>
     <div className='fallback-contact-wrapper-inner'>
-      <h2>Follow me on Social Media</h2>
-      <div className='contact-page-social-inner'>
+      <h2>Contact me on Social Media</h2>
+      <div className='fallback-contact-page-social-inner'>
         <div className='contact-page-social-titles'>
           <span className='contact-page-social-title-item'>Github</span><br />
           <span className='contact-page-social-title-item'>Linkedin</span><br />
@@ -21,10 +21,10 @@ const ContactView = observer(props => (
           <span className='contact-page-social-title-item'>Email</span>
         </div>
         <div className='contact-page-social-symbols'>
-          <FontAwesomeIcon icon={['fab', 'github']} /><br />
-          <FontAwesomeIcon icon={['fab', 'linkedin']} /><br />
-          <FontAwesomeIcon icon={['fab', 'twitter']} /><br />
-          <FontAwesomeIcon icon={'envelope-open'} />
+          <span><FontAwesomeIcon icon={['fab', 'github']} /></span><br />
+          <span><FontAwesomeIcon icon={['fab', 'linkedin']} /></span><br />
+          <span><FontAwesomeIcon icon={['fab', 'twitter']} /></span><br />
+          <span><FontAwesomeIcon icon={'envelope-open'} /></span>
         </div>
         <div className='contact-page-social-links'>
           <a target='_blank' rel='noopener noreferrer' href='https://github.com/intxcc'>intxcc</a><br />
@@ -34,8 +34,8 @@ const ContactView = observer(props => (
         </div>
       </div>
       <div className='contact-page-impressum'>
-        <h2>Angaben gemäß §5 TMG</h2>
-        <p>
+        <h2>Angaben gemäß §5 TMG | Impressum to conform to german law</h2>
+        <p className='contact-page-impressum-content'>
           Marvin Alexander Rüll<br />
           Abtstr. 4<br />
           12489 Berlin

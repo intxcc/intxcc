b5599713952d509bcc563df4dddbec3c07a5fb63
Marvin Alexander Rüll <dev@intx.cc>
2018-10-24 18:59:13 +0200

    Changed text of stories popup.

--- src/config/POPUP_STORIES_EXPLANATION.js
+++ src/config/POPUP_STORIES_EXPLANATION.js
@@ -3,8 +3,8 @@
 const STORIES_EXPLANATION = {
   id: 'stories-explanation-popup',
   className: 'stories-explanation',
-  title: 'Explanation of stories',
-  text: 'Stories means the developer stories I can tell from past or current projects. My portfolio.',
+  title: 'Stories | My Portfolio',
+  text: 'Stories means the development stories I can tell from past or current projects. That is: My portfolio.',
   hint: 'To filter stories for a specific skill, use the stories overlay on the skills-page.'
 }
 

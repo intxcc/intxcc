e021d85ef7616c92cdef751dfa6c471c9f630a84
Marvin Alexander Rüll <dev@intx.cc>
2019-05-13 23:38:00 +0200

    Updated dependencies and fixed bug that happend on big displays, because the guide length was only 2000px.

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -12,7 +12,7 @@ const ProductionDefaults = {
   DefaultStroke: Colors.black,
   DefaultStrokeWidth: 4, // should get multiplied with pixel scale
   DefaultStrokeMiterlimit: '20',
-  GuideLength: 2000,
+  GuideLength: 4000,
   guideStroke: Colors.black,
   enableStartpageBackgroundVideo: true,
   showAllGuides: true,
